5dc2e814c696bf156a37892569db943f01eac102
Shivank33 <118290447+Shivank33@users.noreply.github.com>
2024-02-10 10:44:00 +0000

    changes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";    //treat all JS code as newer version
+
+// alert(3+3);   //we are usimg Nodejs not browser
+
+console.log(3+3);      //Code reuseability should be high
+console.log("Shivank Chaudhary");
+
+let name = "Shivank";
+let age = 20;
+let isLoggedIn = false;
+
+/*     
+Datatypes--->
+number => 2 to power 53
+bigint => for bigger values
+string => ""
+boolean => true/false
+null => standalone value
+undefined => when value is not given to variable
+symbol => unique
+
+object => 
+*/
+
+console.log(typeof "Shivank");    //string
+console.log(typeof age);          //number
+console.log(typeof null);        //object
+console.log(typeof undefined);   //undefined
+
+
