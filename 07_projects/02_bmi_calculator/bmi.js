f6858e213c5dbb36b706b8faa2d76a9932bda868
Shivank33 <118290447+Shivank33@users.noreply.github.com>
2024-02-18 19:20:22 +0000

    changes done

--- 07_projects/02_bmi_calculator/bmi.js
+++ 07_projects/02_bmi_calculator/bmi.js
@@ -11,6 +11,7 @@ form.addEventListener('submit' , function(e){
     const height = parseInt(document.querySelector("#height2").value);
     const weight = parseInt(document.querySelector("#weight2").value);
     const results = document.querySelector("#results");
+    const msg = document.querySelector("#msg");
 
     if(height === '' || height < 0 || isNaN(height)){
         results.innerHTML = `Not valid ${height}`;
@@ -21,5 +22,14 @@ form.addEventListener('submit' , function(e){
     else{
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
         results.innerHTML = bmi;
+        if(bmi < 18.6){
+            msg.innerHTML = "Underweight category";
+        }
+        else if(bmi > 18.6 && bmi < 24.9){
+            msg.innerHTML = "Normalweight category";
+        }
+        else{
+            msg.innerHTML = "OverWeight category";
+        }
     }
 });
